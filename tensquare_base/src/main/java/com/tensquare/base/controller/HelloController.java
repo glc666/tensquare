a3fa160429ad3b426639e052310b78e1850b16d2
glc666 <2277088066@qq.com>
2019-04-11 14:32:39 +0800

    添加了一个hello的方法

---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.tensquare.base.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ayuantalking
+ * @Date: 2019/4/11/0011 14:27
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @RequestMapping()
+    public String hello(){
+        return "hello";
+    }
+}
